75982cbd5d7f3374a6dc83e5d68039504d10b1ec
solomoreal <solomoreal@yahoo.com>
2019-03-22 22:25:06 +0100

    created the service backend. i have issue with opening my editor to commit properly

---
+++ charlsewealthconsulting/app/Http/Controllers/ServiceController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Service;
+use Illuminate\Http\Request;
+
+class ServiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $services = Service::latest()->get();
+        return $services;
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+       //return view('pages.create_services'); 
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $service = new Service;
+        $service->service_name = $request->service_name;
+        $service->save();
+        return $service;
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Service  $service
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Service $service, $id)
+    {
+        $service = Service::findOrFail($id);
+        return $service;   
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Service  $service
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Service $service, $id)
+    {
+        $service = Service::findOrFail($id);
+        return $service;
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Service  $service
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $service = Service::findOrFail($id);
+        $service->service_name = $request->service_name;
+        $service->update();
+        return $service;
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Service  $service
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $service = Service::fiindOrFail($id);
+        $service->delete();
+        return $service;
+    }
+}
