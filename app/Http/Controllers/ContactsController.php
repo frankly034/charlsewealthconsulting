eec2e71d511f44f6cd1837c0db982911f88cc345
solomoreal <solomoreal@yahoo.com>
2019-04-01 23:05:26 +0100

    can now send mail from contact, this is not the final commit

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -1,10 +1,45 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\http\Requests;  
 use Illuminate\Http\Request;
+use App\Service;
+use Mail;
+
 
 class ContactsController extends Controller
 {
-    
+    public function getContactForm(){
+        $services = Service::all();
+
+        //return view('');
+    }
+
+    public function postContact(Request $request){
+        $this->validate($request,[
+            'email' => 'required|email',
+            'subject' => 'min:3',
+            'phone' => 'required',
+            'body' => 'string',
+            'linkin' => 'nullable',
+            'service' => 'nullable',
+            'name' => 'required'
+        ]);
+
+        $data = [
+            'email' => $request->email,
+            'subject' => $request->subject,
+            'phone' => $request->phone,
+            'bodyMessage' => $request->body,
+            'linkin' => $request->linkin,
+            'service' => $request->service,
+            'name' => $request->name
+        ];
+
+        Mail::send('emails.contact', $data, function($message) use ($data){
+            $message->from($data['email']);
+            $message->to('solomoreal@yahoo.com');
+            $message->subject($data['subject']);
+        });
+    }
 }
