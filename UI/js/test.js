14642a15e71870054654711447969174ff6452b7
codxxx <calebCloud94@gmail.com>
2019-04-27 12:34:01 +0100

    Event Gallery Page

---
+++ UI/js/test.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $(".menu-toggle").click(function () {
+      $("nav").toggleClass("active");
+    });
+  
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+      items: 1,
+      loop: true,
+      margin: 20,
+      autoplay: true,
+      autoplayTimeout: 3000,
+      autoplayHoverPause: true
+    });
+    $('.play').on('click', function () {
+      owl.trigger('play.owl.autoplay', [1000])
+    })
+    $('.stop').on('click', function () {
+      owl.trigger('stop.owl.autoplay')
+    })
+  });
+  
+  
